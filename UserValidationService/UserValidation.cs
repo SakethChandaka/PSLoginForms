bcc2e515e63195a5db5a00a7d21e16c1cb3fdf42
Satya Saketh Chandaka <107997956+SakethChandaka@users.noreply.github.com>
2024-09-16 17:40:44 +0530

    Delete UserValidationService directory

--- UserValidationService/UserValidation.cs
+++
@@ -1,8 +0,0 @@
-﻿//using System.ServiceModel;
-
-//[ServiceContract]
-//public interface IUserValidation
-//{
-//    [OperationContract]
-//    (bool isSuccess, string message, string accessToken) ValidateUser(string username, string password);
-//}
